f6e426b6a7b66f8698e88bee9080e455bd642557
sdlee130 <sdlee130@gmail.com>
2022-12-06 21:11:02 +0900

    [FIX] SMS 정보 다른 fragment에 전송하기_2

--- android/app/app/src/main/java/com/example/android/MyReceiver.kt
+++ android/app/app/src/main/java/com/example/android/MyReceiver.kt
@@ -3,11 +3,10 @@ package com.example.android
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.telephony.SmsMessage
 import android.util.Log
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import java.util.*
 
 
@@ -29,18 +28,6 @@ class MyReceiver : BroadcastReceiver() {
                 Log.d("문자 내용", content)
                 Log.d("송신자 번호", sender)
                 Log.d("수신 시간", date)
-
-                val intent = Intent(this, Fragment3:: class.java)
-
-                bundle1.putString("송신자번호", sender)
-                val bundle2 = Bundle()
-                bundle2.putString("수신시간", date)
-                val bundle3 = Bundle()
-                bundle3.putString("문자내용", content)
-
-                val passBundleFragment3 = PassBundleFragment()
-
-
             }
         }
     }
@@ -54,4 +41,18 @@ class MyReceiver : BroadcastReceiver() {
         return messages
     }
 
+    private fun parseSmsMessage(bundle: Bundle): Array<SmsMessage?>? {
+        val objs = bundle["pdus"] as Array<*>?
+        val messages = arrayOfNulls<SmsMessage>(objs!!.size)
+        for (i in objs.indices) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val format = bundle.getString("format")
+                messages[i] = SmsMessage.createFromPdu(objs[i] as ByteArray, format)
+            } else {
+                messages[i] = SmsMessage.createFromPdu(objs[i] as ByteArray)
+            }
+        }
+
+        return messages
+    }
 }
