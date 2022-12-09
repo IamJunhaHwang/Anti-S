c620d0c146fad5afac378a4bb82b62af7bb0c27f
JunHa-Hwang <hwang_junha@naver.com>
2022-12-09 21:41:47 +0900

    [NEW] Load model and tokenizer

--- webapp/app.py
+++ webapp/app.py
@@ -10,6 +10,13 @@ from transformers import AutoModelForSequenceClassification, AutoTokenizer, Auto
 app = Flask(__name__)
 api = Api(app)
 
+MODEL_NAME = '/home/egg2018037024/lab_pycharm/klue_best_model'
+TOK_NAME = '/home/egg2018037024/lab_pycharm/klue_best_tok'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+tokenizer = AutoTokenizer.from_pretrained(TOK_NAME)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+model.to(device)
 
 
 if __name__ == '__main__':
