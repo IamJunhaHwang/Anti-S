d1a8a0f13607747ceff064f7c14e7370e7c6f255
sdlee130 <sdlee130@gmail.com>
2022-12-09 21:50:28 +0900

    [FIX] SMS 정보 다른 fragment에 전송하기_18

--- android/app/app/src/main/java/com/example/android/fragment_3.kt
+++ android/app/app/src/main/java/com/example/android/fragment_3.kt
@@ -7,18 +7,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_3.*
 
 
 class Fragment3 : Fragment() {
-
-
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_1, container, false)
 
-        val passedIntent = getIntent()
+        val passedIntent = requireActivity().intent
         processCommand(passedIntent)
 
         return view
@@ -33,12 +33,6 @@ class Fragment3 : Fragment() {
             editText3.setText(contents)
             editText2.setText(receivedDate)
         }
-    }
-
-
-    protected fun onNewIntent(intent: Intent?) {
-        processCommand(intent)
-        super.onNewIntent(intent)
     }
 
 }
